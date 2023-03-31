2eeb4d0a98a741d7fc39d7af0b79c8bc8fdd7626
Michał Barnaś <mb@semihalf.com>
2023-03-31 18:57:46 +0200

    i2c: add filtering of i2c dumped messages
    This commit adds option to dump i2c messages of only specified
    devices. It makes it easier to debug communication of specific
    i2c device instead of logging all i2c communication.
    The filter of devices is specifiec in device-tree using the
    node with "zephyr,i2c-trace" compatible string.
    
    Example of device-tree node:
    i2c-dump-filter {
    	compatible = "zephyr,i2c-dump-filter";
    	devices = < &display0 >, < &sensor3 >;
    };
    
    Signed-off-by: Michał Barnaś <mb@semihalf.com>

--- drivers/i2c/i2c_common.c
+++ drivers/i2c/i2c_common.c
@@ -25,9 +25,39 @@ void z_i2c_transfer_signal_cb(const struct device *dev,
 }
 #endif
 
+#ifdef CONFIG_I2C_DUMP_MESSAGES_FILTER
+#define DEF_BUS_WITH_ADDR(node, prop, idx) I2C_DT_SPEC_GET(DT_PHANDLE_BY_IDX(node, prop, idx)),
+#define DEF_FILTER_DEV(node)               DT_FOREACH_PROP_ELEM(node, devices, DEF_BUS_WITH_ADDR)
+
+struct i2c_dt_spec messages_filters[] = {
+	DT_FOREACH_STATUS_OKAY(zephyr_i2c_dump_filter, DEF_FILTER_DEV)};
+
+#undef DEF_FILTER_DEV
+#undef DEF_BUS_WITH_ADDR
+#endif
+
 void i2c_dump_msgs_rw(const struct device *dev, const struct i2c_msg *msgs, uint8_t num_msgs,
 		      uint16_t addr, bool dump_read)
 {
+#ifdef CONFIG_I2C_DUMP_MESSAGES_FILTER
+	bool found_dev = 0;
+
+	for (int a = 0; a < ARRAY_SIZE(messages_filters); a++) {
+		struct i2c_dt_spec *filter = &messages_filters[a];
+
+		if (dev != filter->bus || addr != filter->addr) {
+			continue;
+		} else {
+			found_dev = 1;
+			break;
+		}
+	}
+
+	if (!found_dev) {
+		return;
+	}
+#endif
+
 	LOG_DBG("I2C msg: %s, addr=%x", dev->name, addr);
 	for (unsigned int i = 0; i < num_msgs; i++) {
 		const struct i2c_msg *msg = &msgs[i];
